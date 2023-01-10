c62a3af5f1cb87c0cae756a8ca2c31d2930de83e
plitche <kysu728@naver.com>
2023-01-10 21:07:27 +0900

    create project

---
+++ Interface/Thymeleaf/src/main/java/com/example/Thymeleaf/service/ChatService.java
@@ -0,0 +1,18 @@
+package com.example.Thymeleaf.service;
+
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+@Service
+public class ChatService {
+
+    private List<String> applyMemberList = new ArrayList<>();
+
+    public int applyChat(String memberId) {
+        this.applyMemberList.add(memberId);
+        return this.applyMemberList.size();
+    }
+}
